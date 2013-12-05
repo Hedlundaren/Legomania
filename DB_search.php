9b15339dc170333554bf31033cf81ab674769993
Jonathan <jonathan.grangien@gmail.com>
2013-12-05 11:39:54 +0100

    Sökfunktionerna funkar, men får inga bilder till...

--- DB_search.php
+++ DB_search.php
@@ -8,10 +8,19 @@
 </head>
 
 <body>
+
 <!-- Fixa en lista med alternativen för vad man ska söka-->
 <form id="wizard" action="" method="post">
 <p class ="input"><label>
 Search: &nbsp;
+<br>
+<select name="alternativ">
+  <option value="SetID">SetID</option>
+  <option value="Setname">Setname</option>
+  <option value="Year">Year</option>
+  <option value="Categoryname">Categoryname</option>
+</select>
+<br>
 <input type ="text" name ="search" size ="10">
 </label></p>
 
@@ -22,49 +31,57 @@ Search: &nbsp;
 <?php
 if($_POST)
 {
-$connection = mysql_connect ("mysql.itn.liu.se", "lego", "") 
-				or die ("Connection failed!");
+$connection = mysql_connect ("mysql.itn.liu.se", "lego", "")
+                                or die ("Connection failed!");
 
 mysql_select_db ("lego");
 
+$value = $_POST ["alternativ"];
+
 $search = $_POST["search"];
 
-// Alternativ 1: Sök på SetID
 
+// Alternativ 1: Sök på SetID
+if ($value == "SetID")
+{
 $query = "
-(SELECT sets.Setname, sets.SetID
+(SELECT sets.Setname, sets.SetID, sets.Year
 FROM sets
 WHERE sets.SetID = '$search')";
-/*
-// Alternativ 2: Sök på CategoryID
+}
+// Alternativ 2: Sök på Categoryname
 
+if ($value == "Categoryname")
+{
 $query = "
-(SELECT categories.Categoryname, 
-FROM s, p
-WHERE 1)";
+(SELECT sets.Setname, sets.SetID, sets.Year
+FROM sets
+WHERE sets.CatID IN
+						(SELECT categories.CatID
+						FROM categories
+						WHERE categories.Categoryname = '$search'))";
+}
 
-// Alternativ 3: Sök på Categoryname
 
+// Alternativ 3: Sök på Setname
+
+if ($value == "Setname")
+{
 $query = "
-(SELECT categories.Categoryname, 
-FROM s, p
-WHERE 1)";
+(SELECT sets.Setname, sets.SetID, sets.Year
+FROM sets
+WHERE sets.setname = '$search')";
+}
 
-// Alternativ 4: Sök på Setname
+// Alternativ 4: Sök på SetYear
 
+if ($value == "Year")
+{
 $query = "
-(SELECT categories.Categoryname, 
-FROM s, p
-WHERE 1)";
-
-// Alternativ 5: Sök på SetYear 
-
-$query = "
-(SELECT categories.Categoryname, 
-FROM s, p
-WHERE 1)";
-*/
-
+(SELECT sets.Setname, sets.SetID, sets.Year
+FROM sets
+WHERE sets.Year = '$search')";
+}
 
 $contents = mysql_query("$query");
 
@@ -74,8 +91,8 @@ print("<tr>");
 
 for($i = 0; $i < mysql_num_fields($contents); $i++)
 {
-		$fieldname = mysql_field_name($contents, $i);
-			print("<th>$fieldname</th>");
+                $fieldname = mysql_field_name($contents, $i);
+                        print("<th>$fieldname</th>");
 }
 print("</tr>\n");
 
@@ -86,12 +103,29 @@ for($i = 0;$i < mysql_num_fields($contents); $i++)
 {
 print("<td>$row[$i]</td>");
 }
+/*
+   $img_dir = "http://webstaff.itn.liu.se/~stegu76/img.bricklink.com/";
+   $gif_url = $img_dir . "S/" . '$row[1]' . ".gif";
+   $jpg_url = $img_dir . "S/" . '$row[1]' . ".jpg";
+
+   if (fclose(fopen($gif_url, "r")))
+   {
+      print("<p>" . "gif-bild" . "</p>");
+      print("<p><img src='$gif_url' alt='gif-image' /></p>");
+   }
+   else if (fclose(fopen($jpg_url, "r")))
+   {
+      print("<p>" . "jpg-bild" . "</p>"); 
+      print("<p><img src='$jpg_url' alt='jpg-image' /></p>");
+   }
+   else
+   {
+      print("<p>" . "bild saknas" . "</p>"); 
+   }
+*/
 print("</tr>\n");
 }
 print("</table>\n");
-
-
-
 
 
 mysql_close($connection);
