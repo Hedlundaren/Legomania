5f6b501f224d900b07649ca1ca29e7c207808e53
Adam Alsegård <adam.alsegard@gmail.com>
2013-12-12 18:43:28 +0100

    Update DB_search.php

--- DB_search.php
+++ DB_search.php
@@ -3,14 +3,16 @@
 <head>
 <meta charset="utf-8">
 <title>Legomania</title>
+<link rel="stylesheet" type="text/css" href="style.css">
 
 </head>
 
 <body>
 
 <!--en lista med alternativen för vad man ska söka--> 
-<form name="wizard" action="" method="post" onsubmit="validate()">
-<p class ="input"><label>
+<header>
+<form name="wizard" action="DB_search.php" method="post" onsubmit="return validate()">
+<p id="input"><label>
 Search sets by: &nbsp;
 <br>
 <select name="alternativ">
@@ -22,15 +24,21 @@ Search sets by: &nbsp;
 </label></p>
 
 <!--sökfält-->
+<p id="input2">
 <input type ="text" name ="search" placeholder = "Enter input!" size ="20"/>
 <br/>
 <input type = "submit" value = "Find lego!"/>
 <input type = "reset" value = "Clear"/>
-<br>
-Show images: <input type = "radio" name = "image" value = "Show"> 
-Hide images: <input type = "radio" name = "image" value = "Hide"> 
+Show images: <input type = "checkbox" name = "image" value = "Show"> 
+</p>
 </form>
 
+<!--
+<div id="forum">
+<a href="http://facebook.com/">Forum</a>
+</div>
+-->
+</header>
 
 <!-- Fråga hur man bäst validerar, och hur man gör för att det inte ska gå vidare! -->
 <script>
@@ -41,10 +49,14 @@ Hide images: <input type = "radio" name = "image" value = "Hide">
     if (form == "Enter input!" || form == "Null" || form == "")
 		{
 			window.alert("Please enter input!");
-			break;
+			return false;
 		}
+		return true;
 	}
 </script>
+
+<img id="background" src="background.jpg">
+<div class="opacitybox">
 
 <?php 	
 if($_POST)
@@ -55,7 +67,6 @@ $connection = mysql_connect ("mysql.itn.liu.se", "lego", "")
 mysql_select_db ("lego");
 
 $value = $_POST ["alternativ"];
-$value_image = $_POST["image"];
 
 $search = $_POST["search"];
 
@@ -113,9 +124,9 @@ for($i = 0; $i < mysql_num_fields($contents); $i++)
                 $fieldname = mysql_field_name($contents, $i);
                         print("<th>$fieldname</th>");
 }
-if ($value_image == "Show"){
- print("<th>Image</th>");
-}
+//if ($value_image == true){
+ print("<th class='pictureColumn'>Image</th>");
+//}
 print("</tr>\n");
 
 while($row = mysql_fetch_row($contents))
@@ -126,7 +137,8 @@ for($i = 1;$i < mysql_num_fields($contents); $i++)
 {
 print("<td>$row[$i]</td>");
 }
-if ($value_image == "Show"){
+
+//if ($value_image == "Show"){
 $img_dir = "http://webstaff.itn.liu.se/~stegu76/img.bricklink.com/";
 $gif_url = $img_dir . 'S/' . $row[1] . '.gif';  //'$row[1]'
 $jpg_url = $img_dir . 'S/' . $row[1] . '.jpg';
@@ -134,17 +146,17 @@ $jpg_url = $img_dir . 'S/' . $row[1] . '.jpg';
 if(@fclose(@fopen($gif_url, "r"))){
 //print("<td>" . "" . "</td>");
 
-print("<td><a href='$gif_url'> <img src='$gif_url' alt='gif-image' /></a></td>");
+print("<td class='pictureColumn'><a href='$gif_url'> <img src='$gif_url' alt='gif-image' /></a></td>");
 
 }
 else if(@fclose(@fopen($jpg_url, "r"))){
 //print("<p>" . "jpg-bild" . "</p>");
-print("<td><a href='$jpg_url'><img src='$jpg_url' alt='jpg-image' /></a></td>");
+print("<td class='pictureColumn'><a href='$jpg_url'><img src='$jpg_url' alt='jpg-image' /></a></td>");
 }
 else{
-print("<td>" . "bild saknas" . "</td>");
+print("<td class='pictureColumn'>" . "bild saknas" . "</td>");
 }
-}
+//}
 
 print("</tr>\n");
 }
@@ -154,6 +166,11 @@ print("</table>\n");
 mysql_close($connection);
 }
 ?>
+</div>
+<footer>
+</footer>
 
+<script type="text/javascript" src="jquery-1.10.2.js"></script>
+<script type="text/javascript" src="validate.js"></script>
 </body>
 </html>
