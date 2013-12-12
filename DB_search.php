cf28b866bd50d0d391ccdd87bb48e0b617aa073f
Adam Alsegård <adam.alsegard@gmail.com>
2013-12-12 14:56:54 +0100

    Update DB_search.php
    Sök efter liknande, radio buttons, valideringsförsök

--- DB_search.php
+++ DB_search.php
@@ -4,15 +4,14 @@
 <meta charset="utf-8">
 <title>Legomania</title>
 
-
 </head>
 
 <body>
 
-<!--en lista med alternativen för vad man ska söka-->
-<form id="wizard" action="" method="post">
+<!--en lista med alternativen för vad man ska söka--> 
+<form name="wizard" action="" method="post" onsubmit="validate()">
 <p class ="input"><label>
-Search: &nbsp;
+Search sets by: &nbsp;
 <br>
 <select name="alternativ">
 <option value="SetID">SetID</option>
@@ -23,13 +22,31 @@ Search: &nbsp;
 </label></p>
 
 <!--sökfält-->
-<input type ="text" name ="search" size ="20"/>
+<input type ="text" name ="search" placeholder = "Enter input!" size ="20"/>
 <br/>
 <input type = "submit" value = "Find lego!"/>
 <input type = "reset" value = "Clear"/>
+<br>
+Show images: <input type = "radio" name = "image" value = "Show"> 
+Hide images: <input type = "radio" name = "image" value = "Hide"> 
 </form>
 
-<?php
+
+<!-- Fråga hur man bäst validerar, och hur man gör för att det inte ska gå vidare! -->
+<script>
+   function validate(){
+	
+	var form = document.forms["wizard"]["search"].value;
+
+    if (form == "Enter input!" || form == "Null" || form == "")
+		{
+			window.alert("Please enter input!");
+			break;
+		}
+	}
+</script>
+
+<?php 	
 if($_POST)
 {
 $connection = mysql_connect ("mysql.itn.liu.se", "lego", "")
@@ -38,6 +55,7 @@ $connection = mysql_connect ("mysql.itn.liu.se", "lego", "")
 mysql_select_db ("lego");
 
 $value = $_POST ["alternativ"];
+$value_image = $_POST["image"];
 
 $search = $_POST["search"];
 
@@ -48,7 +66,7 @@ if ($value == "SetID")
 $query = "
 (SELECT sets.Setname, sets.SetID, sets.Year
 FROM sets
-WHERE sets.SetID = '$search')";
+WHERE sets.SetID LIKE '%$search%')";
 }
 // Alternativ 2: Sök på Categoryname
 
@@ -58,9 +76,9 @@ $query = "
 (SELECT sets.Setname, sets.SetID, sets.Year
 FROM sets
 WHERE sets.CatID IN
-                                                (SELECT categories.CatID
-                                                FROM categories
-                                                WHERE categories.Categoryname = '$search'))";
+                    (SELECT categories.CatID
+                     FROM categories
+                     WHERE categories.Categoryname LIKE '%$search%'))";
 }
 
 
@@ -71,7 +89,7 @@ if ($value == "Setname")
 $query = "
 (SELECT sets.Setname, sets.SetID, sets.Year
 FROM sets
-WHERE sets.setname = '$search')";
+WHERE sets.setname LIKE '%$search%')";
 }
 
 // Alternativ 4: Sök på SetYear
@@ -95,17 +113,20 @@ for($i = 0; $i < mysql_num_fields($contents); $i++)
                 $fieldname = mysql_field_name($contents, $i);
                         print("<th>$fieldname</th>");
 }
+if ($value_image == "Show"){
  print("<th>Image</th>");
+}
 print("</tr>\n");
 
 while($row = mysql_fetch_row($contents))
 {
 print("<tr>");
-for($i = 0;$i < mysql_num_fields($contents); $i++)
+print("<td><a href='suppliers.php?setID=$row[1]'>$row[0]</a></td>");
+for($i = 1;$i < mysql_num_fields($contents); $i++)
 {
 print("<td>$row[$i]</td>");
 }
-
+if ($value_image == "Show"){
 $img_dir = "http://webstaff.itn.liu.se/~stegu76/img.bricklink.com/";
 $gif_url = $img_dir . 'S/' . $row[1] . '.gif';  //'$row[1]'
 $jpg_url = $img_dir . 'S/' . $row[1] . '.jpg';
@@ -113,18 +134,17 @@ $jpg_url = $img_dir . 'S/' . $row[1] . '.jpg';
 if(@fclose(@fopen($gif_url, "r"))){
 //print("<td>" . "" . "</td>");
 
-print("<td> <img src='$gif_url' alt='gif-image' /></td>");
+print("<td><a href='$gif_url'> <img src='$gif_url' alt='gif-image' /></a></td>");
 
 }
 else if(@fclose(@fopen($jpg_url, "r"))){
 //print("<p>" . "jpg-bild" . "</p>");
-print("<td><img src='$jpg_url' alt='jpg-image' /></td>");
+print("<td><a href='$jpg_url'><img src='$jpg_url' alt='jpg-image' /></a></td>");
 }
 else{
 print("<td>" . "bild saknas" . "</td>");
 }
-
-
+}
 
 print("</tr>\n");
 }
