3e215d7fc99f91c5fc9003e970f33cbedb4baf41
Adam Alsegård <adam.alsegard@gmail.com>
2013-12-16 16:55:18 +0100

    Viktigt!

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+<!--Slut opacity-taggen-->
+</div>
+
+<footer>
+<script type="text/javascript" src="jquery-1.10.2.js"></script>
+<script type="text/javascript" src="magic.js"></script>
+</footer>
+
+</body>
+</html>
