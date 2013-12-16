8b5c1995c31f06b9cd7be9eaac5c7c5a3d5ef32f
Adam Alsegård <adam.alsegard@gmail.com>
2013-12-16 16:47:14 +0100

    Viktigt! JQuery-funktioner

--- magic.js
+++ magic.js
@@ -1,10 +1,19 @@
 $(".pictureColumn").hide();
 
-$("input[type='checkbox']")
-	.change(function(){
-		if($(this).is(":checked"))
-			$(".pictureColumn").show();
-		else
-			$(".pictureColumn").hide();
-	});
-	
+$("input[name=image]")
+        .change(function(){
+                if($(this).is(":checked"))
+                        $(".pictureColumn").show();
+                else
+                        $(".pictureColumn").hide();
+        });
+		
+$(".parts").hide();
+
+$("input[name=parts]")
+        .change(function(){
+                if($(this).is(":checked"))
+                        $(".parts").show();
+                else
+                        $(".parts").hide();
+        });
