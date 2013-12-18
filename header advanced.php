6bec45ff162b26df0cc7412fb484fc3caf4d492c
Jonathan <jonathan.grangien@gmail.com>
2013-12-18 11:52:14 +0100

    Har massa br-taggar tills mysql-frågan i advanced.php funkar

--- header advanced.php
+++ header advanced.php
@@ -9,20 +9,21 @@
 <body>
 
 <!--Advance search--> 
-<header>
+<header><br><br><br><br><br><br><br><br><br><br>
 <form name="wizard" action="advanced.php" method="post">
-	<p id="input"><label>
-	<input type ="text" name = "setID" placeholder = "Enter SetID" size = "10"/>
-	<input type ="text" name = "year" placeholder = "Enter year" size = "10"/>
-	<input type ="text" name = "setname" placeholder = "Enter Setname" size = "10"/>
-	<input type ="text" name = "categoryname" placeholder = "Enter Categoryname" size = "10"/>
-	<input type ="text" name = "catID" placeholder = "Enter CategoryID" size = "10"/>
-	<input type ="text" name = "partID" placeholder = "Enter PartID" size = "10"/>
-	<br>
-	</label></p>
+	<p id="input"><!--<label>-->
+	<input type ="text" name = "SetID" placeholder = "Enter SetID" size = "10"/>
+	<input type ="text" name = "Year" placeholder = "Enter year" size = "10"/>
+	<input type ="text" name = "Setname" placeholder = "Enter Setname" size = "10"/>
+	<input type ="text" name = "Categoryname" placeholder = "Enter Categoryname" size = "10"/>
+	<input type ="text" name = "CatID" placeholder = "Enter CategoryID" size = "10"/>
+	<input type ="text" name = "PartID" placeholder = "Enter PartID" size = "10"/>
+	<!--</label>--><br>
+	
 
-	<input type = "submit" value = "Find lego!"/>
+	<input type = "submit" value = "Find!"/>
 	<input type = "reset" value = "Clear"/>
+	
 	Show images: <input type = "checkbox" name = "image" value = "Show"> 
 	</p>
 </form>
