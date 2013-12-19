5e1b0b217cbeabf01f8d2d9bca2ba58fbaf2ff38
liuloppan <lovha997@student.liu.se>
2013-12-19 18:57:38 +0100

    kommenterad och halvt rensad

--- header.php
+++ header.php
@@ -8,66 +8,74 @@
 
 <body>
 
-<!--en lista med alternativen för vad man ska söka-->
-<header>
+	<header>
 
-<div id="fade">
-<img id="headerpic" src="bakgrundopacity.png">
-</div>
+		<!--bakgrund i header-->
+		<div id="fade">
+		<img id="headerpic" src="bakgrundopacity.png"> 
+		</div>
 
+		<!--innehåller alla element i header-->
+		<form id ="wizard" name="wizard" action="search.php" method="get" onsubmit="return validate()"> 
+			<!--länk till startsidan-->
+			<a id="home" href="https://www.facebook.com/"> 
+			<img src="homebutton.png"> </a>
 
-<form id ="wizard" name="wizard" action="search.php" method="get" onsubmit="return validate()">
-<a id="home" href="https://www.facebook.com/">
-<img src="homebutton.png"> </a>
+			<!--Forumlänk-->
+			<a id="forum" href="http://www.sermon.se/legoforum">
+			<img src="forumbutton12.png"></a> 
 
-<a id="forum" href="http://www.sermon.se/legoforum"><img src="forumbutton12.png"></a>
+				<label>
+					<div class="litentext">Search sets by: &nbsp;
+					</div>
+					<!--en lista med alternativen för vad man ska söka-->
+						<select name="alternativ">  
+							<option value="SetID">SetID</option>
+							<option value="Setname">Setname</option>
+							<option value="Year">Year</option>
+							<option value="Categoryname">Categoryname</option>
+						</select>
+				</label>
 
-<label>
-<div class="litentext">Search sets by: &nbsp;
-</div>
-<select name="alternativ">
-<option value="SetID">SetID</option>
-<option value="Setname">Setname</option>
-<option value="Year">Year</option>
-<option value="Categoryname">Categoryname</option>
-</select>
-</label>
+	<!--sökfält-->
+			<br>
+			<input type ="text" name ="search" placeholder = "Enter input!" />
+			<br/>
+			
+			<!--knappar-->
+			<input type = "submit" value = "Find"/>
+			<input type = "reset" value = "Clear"/>
+			
+			<div class="litentext">Show images: <input type = "checkbox" name = "image" value = "Show">
+			</div>
 
-<!--sökfält-->
-<p id="input2">
-<input type ="text" name ="search" placeholder = "Enter input!" size ="20"/>
-<br/>
-<input type = "submit" value = "Find"/>
-<input type = "reset" value = "Clear"/>
-<div class="litentext">Show images: <input type = "checkbox" name = "image" value = "Show">
-</div>
-</p>
-</form>
+		</form>
 
-<!--<a href="advanced.php">Advanced search</a>-->
-<!--
-<div id="forum">
-<a href="http://facebook.com/">Forum</a>
-</div>
--->
-</header>
+		<!--Avancerad sök-->
+		<div id="advanced">
+		<a href = "advanced.php"> Advance search 
+		</div>
 
-<!-- Fråga hur man bäst validerar, och hur man gör för att det inte ska gå vidare! -->
-<script>
-function validate(){
-var form = document.forms["wizard"]["search"].value;
+	</header>
 
-if (form == "Enter input!" || form == "Null" || form == "")
-{
-window.alert("Please enter input!");
-return false;
-}
-return true;
-}
-</script>
+	<!-- Fråga hur man bäst validerar, och hur man gör för att det inte ska gå vidare! -->
+	<script>
+	function validate(){
+	var form = document.forms["wizard"]["search"].value;
 
-<img id="background" src="bakgrund3.png">
-<div class="opacitybox">
+	if (form == "Enter input!" || form == "Null" || form == "")
+	{
+	window.alert("Please enter input!");
+	return false;
+	}
+	return true;
+	}
+	</script>
+
+	<!--Bakgrund i body-->
+	<img id="background" src="bakgrund3.png">
+	 <!--opacity för tabell-->
+	<div class="opacitybox">
 
 <?php
         $connection = mysql_connect ("mysql.itn.liu.se", "lego", "")
@@ -77,3 +85,4 @@ return true;
         
         mysql_select_db ("lego");
 ?>
+
