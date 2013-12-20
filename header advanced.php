8eee5d119cd1ccdf1b9c2eebc1e1b0e1e4d873ae
Jonathan <jonathan.grangien@gmail.com>
2013-12-20 11:49:15 +0100

    med div-taggar för wrapper, header..

--- header advanced.php
+++ header advanced.php
@@ -7,28 +7,29 @@
 </head>
 
 <body>
+<div id="wrapper">
+		<div id="header">
+		<!--Advance search--> 
+		<form name="wizard" action="advanced.php" method="get">
+			<p id="input"><!--<label>-->
+			<input type ="text" name = "SetID" placeholder = "Enter SetID" size = "10"/>
+			<input type ="text" name = "Year" placeholder = "Enter year" size = "10"/>
+			<input type ="text" name = "Setname" placeholder = "Enter Setname" size = "10"/>
+			<input type ="text" name = "Categoryname" placeholder = "Enter Categoryname" size = "10"/>
+			<input type ="text" name = "CatID" placeholder = "Enter CategoryID" size = "10"/>
+			<input type ="text" name = "PartID" placeholder = "Enter PartID" size = "10"/>
+			<!--</label>--><br>
+			
 
-<!--Advance search--> 
-<header><br><br><br><br><br><br><br><br><br><br>
-<form name="wizard" action="advanced.php" method="get">
-	<p id="input"><!--<label>-->
-	<input type ="text" name = "SetID" placeholder = "Enter SetID" size = "10"/>
-	<input type ="text" name = "Year" placeholder = "Enter year" size = "10"/>
-	<input type ="text" name = "Setname" placeholder = "Enter Setname" size = "10"/>
-	<input type ="text" name = "Categoryname" placeholder = "Enter Categoryname" size = "10"/>
-	<input type ="text" name = "CatID" placeholder = "Enter CategoryID" size = "10"/>
-	<input type ="text" name = "PartID" placeholder = "Enter PartID" size = "10"/>
-	<!--</label>--><br>
-	
+			<input type = "submit" value = "Find!"/>
+			<input type = "reset" value = "Clear"/>
+			
+			Show images: <input type = "checkbox" name = "image" value = "Show"> 
+			</p>
+		</form>
+	</div><!--sluttagg header-->
+	<div id="content">
 
-	<input type = "submit" value = "Find!"/>
-	<input type = "reset" value = "Clear"/>
-	
-	Show images: <input type = "checkbox" name = "image" value = "Show"> 
-	</p>
-</form>
-</header>
-
-<img id="background" src="background.jpg">
-<div class="opacitybox">
+		<img id="background" src="background.jpg">
+		<div class="opacitybox">
 
