9f492317639a663a4c76a6602c0dc2fcb8c66e75
Jonathan <jonathan.grangien@gmail.com>
2013-12-20 11:51:20 +0100

    med sluttaggar för divvarna

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
+	</div>	<!--Sluttagg opacity-->
+		
+	</div>	<!--sluttagg content-->
+		<div id="footer">
+		<script type="text/javascript" src="jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="magic.js"></script>
+		</div>
 
-<!--Slut opacity-taggen-->
-</div>
-
-<footer>
-<script type="text/javascript" src="jquery-1.10.2.js"></script>
-<script type="text/javascript" src="magic.js"></script>
-</footer>
-
+</div>		<!--sluttagg wrapper-->
 </body>
 </html>
