37b986ac38178fe6ac1c96062634d27be4614d4b
Adam Alsegård <adam.alsegard@gmail.com>
2014-01-06 17:36:48 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,19 +1,32 @@
 <?php
 include "header.php";
 ?>
-<link rel="stylesheet" type="text/css" href="style_main.css"/>
+</div><!--Sluttagg opacitybox-->
+
 <div id="welcome">
-<p class="mainText">Welcome!</p>
-<p class="mainText">This is a site for all you lego maniacs out there in need of a more convenient way 
-	to find specific information about lego sets. </p>
-<p class="mainText">Use the search function above to find the sets you are looking for.
-   Once results are shown you can click on a desired set to display more information about it and
-   its included lego parts. </p>
+	<p class="mainText">Welcome!</p>
+	<p class="mainText">This is a site for all you lego maniacs out there in need of a more convenient way
+			to find specific information about lego sets. </p>
+	<p class="mainText">Use the search function above to find the sets you are looking for.
+	Once results are shown you can click on a desired set to display more information about it and
+	its included lego parts. </p>
 </div>
 
-<!--bild-->
-<img id="mainImg" src="main_picture3.png" alt="lego picture"/>
 
+<!--bild-->
+<div id="mainImg">
+<img src="pictures/main_picture3.png" alt="lego picture"/>
+</div>
+
+	<p class = "validator">
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">
+			<img style="border:0;width:88px;height:31px"
+				src="http://jigsaw.w3.org/css-validator/images/vcss"
+				alt="Valid CSS!" />
+		</a>
+	</p>
+
+	<div><!--Ny opacitybox-->
 <?php
 include "footer.php";
 ?>
