cdf71350c964aa6912715e15667f557544de6edb
Jonathan <jonathan.grangien@gmail.com>
2014-01-06 14:01:44 +0100

    Nya funktioner till frågetecknena

--- magic.js
+++ magic.js
@@ -1,23 +1,41 @@
-$(".pictureColumn").show();
-
 $("input[name=image]")
-        .change(function(){
-                if($(this).is(":checked"))
-                        $(".pictureColumn").show();
-                else
-                        $(".pictureColumn").hide();
-        });
+    .change(function(){
+        if($(this).is(":checked"))
+            $(".pictureColumn").show();
+        else
+            $(".pictureColumn").hide();
+    });
                 
 $(".parts").hide();
 
 $("input[name=parts]")
-        .change(function(){
-                if($(this).is(":checked"))
-                        $(".parts").show();
-                else
-                        $(".parts").hide();
-        });
+    .change(function(){
+        if($(this).is(":checked"))
+            $(".parts").show();
+        else
+            $(".parts").hide();
+    });
 		
 $(function() {
-$( document ).tooltip();
+	$( document ).tooltip();
+});
+
+//bildbyte för frågetecknet i huvudsökningen
+$("#question").on({
+ "mouseover" : function() {
+    this.src = 'Questionmark3_hover.png';
+	},
+  "mouseout" : function() {
+    this.src='Questionmark3.png';
+    }
+});
+
+//bildbyte för frågetecknet i den avancerade sökningen
+$("#questionAdv").on({
+ "mouseover" : function() {
+    this.src = 'Questionmark_adv_hover.png';
+    },
+  "mouseout" : function() {
+    this.src='Questionmark_adv.png';
+    }
 });
