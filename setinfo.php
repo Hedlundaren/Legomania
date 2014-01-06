1433cda9983d5e99e95b8b580405708c6183122e
Adam Alsegård <adam.alsegard@gmail.com>
2014-01-06 17:37:40 +0100

    Update setinfo.php

--- setinfo.php
+++ setinfo.php
@@ -3,7 +3,7 @@ include "header.php";
 
 $setID = $_GET["setID"];
 
-
+//Räkna ut hur många bitar setet har samt ta fram namn och år
 $quest = "(
 	SELECT DISTINCT sets.Setname, sets.Year, SUM(inventory.Quantity) AS total
 	FROM sets, inventory
@@ -14,106 +14,36 @@ $quest = "(
 $konsum = mysql_query("$quest");
 $kolumn = mysql_fetch_row($konsum);
 
-//Skriv ut namnet på valt set, år, antal delar samt beräknad tidsåtgång 
+//Skriv ut namnet på valt set, år, antal delar samt beräknad tidsåtgång; antal bitar/2
 print ("<table><tr>");
-print ("<td><h2>'$kolumn[0]'</h2></td>");
-print ("<tr><td><h3>$kolumn[1]</h3></td></tr>");
-$qvant = $kolumn[2]/2;
-print ("<tr><td><h3>Estimated time required: $qvant min </h3></td></tr></tr><tr>");
-//Visa en stor bild på valt set
-			$img_dir = "http://webstaff.itn.liu.se/~stegu76/img.bricklink.com/";
-			$gif_url = $img_dir . 'SL/' . $setID . '.gif';
-			$jpg_url = $img_dir . 'SL/' . $setID . '.jpg';
+	print ("<td><h2>'$kolumn[0]'</h2></td>");
+	print ("<tr><td><h3>$kolumn[1]</h3></td></tr>");
+	$qvant = $kolumn[2]/2;
+	print ("<tr><td><h3>Estimated time required: $qvant min </h3></td></tr></tr><tr>");
 
-			if(@fclose(@fopen($gif_url, "r"))){
-				print("<td><img src='$gif_url' alt='gif-image' /></td>");
-			}
-			
-			else if(@fclose(@fopen($jpg_url, "r"))){
-				print("<td><img src='$jpg_url' alt='jpg-image' /></td>");
-			}
-			
-			else{
-				print("<td>" . "No Image" . "</td>");
-			}
-
-			print ("</tr></table>");
-			print ("<div  class='litentext' id='showparts'>Show parts: <input type = 'checkbox' name = 'parts' value = 'Show'></div>");	
-//Välj ifall du ska visa delarna i valt set
-
-
-
-// Visa alla delar i valt set 
-$query = "(
-	SELECT DISTINCT parts.partname, parts.PartID, inventory.Quantity, colors.Colorname, colors.ColorID
-	FROM parts, inventory, colors
-	WHERE  inventory.SetID = '$setID'
-		AND parts.PartID = inventory.ItemID
-		AND colors.ColorID = inventory.ColorID)";
-
-$contents = mysql_query("$query");
-
-print("<table id='setinfotable' class='parts' border='border' cellpadding='6' cellspacing='3'>\n");
-	print("<tr>");
-	print("<th class='pictureColumn'>Image</th>");
-
-	for($i = 0; $i < mysql_num_fields($contents) - 1; $i++){
-		$fieldname = mysql_field_name($contents, $i);
-		print("<th>$fieldname</th>");
-	}
-
-	print("</tr>\n");
-
-	while($row = mysql_fetch_row($contents)){
-		print("<tr>");
+//Visa, om möjligt, en stor bild på valt set
 		$img_dir = "http://webstaff.itn.liu.se/~stegu76/img.bricklink.com/";
-		$gif_url = $img_dir . 'P/' . $row[4] . '/' . $row[1] . '.gif'; 
-		$jpg_url = $img_dir . 'P/' . $row[4] . '/' . $row[1] . '.jpg';
-		
-			$gifbig_url = $img_dir . 'PL/' . $row[1] . '.gif'; 
-			$jpgbig_url = $img_dir . 'PL/' . $row[1] . '.jpg';
+		$gif_url = $img_dir . 'SL/' . $setID . '.gif';
+		$jpg_url = $img_dir . 'SL/' . $setID . '.jpg';
 
 		if(@fclose(@fopen($gif_url, "r"))){
-			if(@fclose(@fopen($gifbig_url, "r"))){
-				print("<td class='pictureColumn'><a href='$gifbig_url'> 
-				<img src='$gif_url' alt='gif-image' /></a></td>");
-			}
-			else if(@fclose(@fopen($jpgbig_url, "r"))){
-				print("<td class='pictureColumn'><a href='$jpgbig_url'> 
-				<img src='$gif_url' alt='gif-image' /></a></td>");
-			}
-			else{
-				print("<td class='pictureColumn'><img src='$gif_url' alt='gif-image' /></td>");
-			}
+			print("<td><img src='$gif_url' alt='gif-image' /></td>");
 		}
-
+		
 		else if(@fclose(@fopen($jpg_url, "r"))){
-			if(@fclose(@fopen($gifbig_url, "r"))){
-				print("<td class='pictureColumn'><a href='$gifbig_url'> 
-				<img src='$jpg_url' alt='jpg-image' /></a></td>");
-			}
-			else if(@fclose(@fopen($jpgbig_url, "r"))){
-				print("<td class='pictureColumn'><a href='$jpgbig_url'> 
-				<img src='$jpg_url' alt='jpg-image' /></a></td>");
-			}
-			else{
-				print("<td class='pictureColumn'><img src='$jpg_url' alt='jpg-image' /></td>");
-			}
+			print("<td><img src='$jpg_url' alt='jpg-image' /></td>");
 		}
-
+		
 		else{
-			print("<td class='pictureColumn'>" . "No Image" . "</td>");
+			print("<td>" . "No Image" . "</td>");
 		}
 
-		for($i = 0;$i < mysql_num_fields($contents) -1; $i++){
-			print("<td>$row[$i]</td>");
-		}
+print ("</tr></table>");
 
-		print("</tr>\n");
-	}
+//Välj ifall du vill se delarna i valt set
+print ("<div  class='litentext' id='showparts'>Show parts: <input type = 'checkbox' name = 'parts' value = 'Show'></div>");	
 
-print("</table>\n");
-
-mysql_close($connection);
+// Visa alla delar i valt set 
+include "searchfunctions/showparts.php";
 
 include "footer.php";
