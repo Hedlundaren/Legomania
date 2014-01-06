274794b38f4b2724743bf595d5e2b36adad5a366
Adam Alsegård <adam.alsegard@gmail.com>
2014-01-06 17:36:26 +0100

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,41 +1,44 @@
+
+// Visa/göm bilderna beroende på om checkboxen är iklickad
 $("input[name=image]")
-    .change(function(){
-        if($(this).is(":checked"))
-            $(".pictureColumn").show();
-        else
-            $(".pictureColumn").hide();
-    });
+        .change(function(){
+                if($(this).is(":checked"))
+                        $(".pictureColumn").show();
+                else
+                        $(".pictureColumn").hide();
+        });
                 
 $(".parts").hide();
 
 $("input[name=parts]")
-    .change(function(){
-        if($(this).is(":checked"))
-            $(".parts").show();
-        else
-            $(".parts").hide();
-    });
+        .change(function(){
+                if($(this).is(":checked"))
+                        $(".parts").show();
+                else
+                        $(".parts").hide();
+        });
 		
+//Visa hjälpkommentarerna när användaren hovrar över frågetecknet
 $(function() {
-	$( document ).tooltip();
+        $( document ).tooltip();
 });
 
 //bildbyte för frågetecknet i huvudsökningen
 $("#question").on({
  "mouseover" : function() {
-    this.src = 'Questionmark3_hover.png';
-	},
+    this.src = 'pictures/Questionmark3_hover.png';
+        },
   "mouseout" : function() {
-    this.src='Questionmark3.png';
+    this.src = 'pictures/Questionmark3.png';
     }
 });
 
 //bildbyte för frågetecknet i den avancerade sökningen
 $("#questionAdv").on({
  "mouseover" : function() {
-    this.src = 'Questionmark_adv_hover.png';
+    this.src = 'pictures/Questionmark_adv_hover.png';
     },
   "mouseout" : function() {
-    this.src='Questionmark_adv.png';
+    this.src = 'pictures/Questionmark_adv.png';
     }
 });
