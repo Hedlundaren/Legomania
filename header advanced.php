2eaf5c6b4468964a5170066ed4a86f5e39d20ad9
liuloppan <lovha997@student.liu.se>
2014-01-06 14:20:13 +0100

    stylad advance

--- header advanced.php
+++ header advanced.php
@@ -10,27 +10,40 @@
 <div id="wrapper">
 		<div id="header">
 		<!--Advance search--> 
-		<form name="wizard" action="advanced.php" method="get">
-			<p id="input"><!--<label>-->
+		<form id ="wizard" name="wizard" action="advanced.php" method="get">
+		
+					<!--länk till startsidan-->
+                        <a id="home" href="https://www.facebook.com/">
+                        <img src="homebutton.png"> </a>
+
+                        <!--Forumlänk-->
+                        <a id="forum" href="http://www.sermon.se/legoforum">
+                        <img src="forumbutton12.png"></a>
+						
+					<p id="input"><!--<label>-->
 			<input type ="text" name = "SetID" placeholder = "Enter SetID" size = "10"/>
 			<input type ="text" name = "Year" placeholder = "Enter year" size = "10"/>
 			<input type ="text" name = "Setname" placeholder = "Enter Setname" size = "10"/>
+			</br>
 			<input type ="text" name = "Categoryname" placeholder = "Enter Categoryname" size = "10"/>
 			<input type ="text" name = "CatID" placeholder = "Enter CategoryID" size = "10"/>
 			<input type ="text" name = "PartID" placeholder = "Enter PartID" size = "10"/>
 			<!--</label>--><br>
 			
 
-			<input type = "submit" value = "Find!"/>
+			<input type = "submit" value = "Find"/>
 			<input type = "reset" value = "Clear"/>
-			
-			Show images: <input type = "checkbox" name = "image" value = "Show" checked> 
-			</p>
-		</form>
-		<img id="questionAdv" src="Questionmark_adv.png" alt="questionmark" 
+			<img id="questionAdv" src="Questionmark_adv.png" alt="questionmark" 
 			title="You can use advanced search to find sets according to more specific details by inputting multiple pieces of information. When searching for the setname or categoryname the search will display similar results to your input, for the other attributes the search terms need to be exact."/>
+			
+			<div class="litentext">
+                                      Show images: <input type = "checkbox" name = "image" value = "Show"> 
+			</div></p>
+			
+		</form>
 		
-		</div><!--sluttagg header-->
+
+   </div><!--sluttagg header-->
 	<div id="content">
 
 		<img id="background" src="background.jpg">
